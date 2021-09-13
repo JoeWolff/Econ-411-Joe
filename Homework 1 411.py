384706dfb3772f21a918b70fe5923055b38d71db
JoeWolff <joseph.wolff@ndsu.edu>
2021-09-12 22:27:52 -0500

    Stuff

---
+++ Homework 1 411.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#1
+x="Joe Wolff"
+y="mechanical engeering"
+print(x,y)
+
+
+# In[16]:
+
+
+#2
+twotothefourth=2^4
+z=2*twotothefourth
+q=twotothefourth
+W=twotothefourth+twotothefourth
+print(twotothefourth)
+print(z)
+print(q)
+print(W)
+
+
+# In[35]:
+
+
+#3
+l="this\n that"
+print(l)
+E="This\'s there"
+print(E)
+d="backslash \\"
+print(d)
+m="space\tbetween"
+print(m)
+
+
+# In[37]:
+
+
+#4
+T=8
+R=4.3
+Q=T+R
+print(Q)
+#the answer is an integer 
+
+
+# In[38]:
+
+
+x=2*1024
+y=x+1.5
+print(y)
+#the outcome is 2049.5 this is because the first is 2048
+
+
+# In[46]:
+
+
+#6
+import string
+print (string.__dict__)
+#the output is the copyright
+
+
+# In[54]:
+
+
+#7
+'abs'
+'open'
+
+
+# In[49]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Homework 2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+#1
+a=range(3,301,3)
+print(*a)
+
+
+# In[24]:
+
+
+#2
+a = map(str, a)
+print(list(a))
+
+
+# In[25]:
+
+
+#3
+int_list = range(3,301,3)
+
+print(int_list)
+
+
+# In[28]:
+
+
+#4
+c  = a
+c.reverse()
+print(c)
+
+
+# In[29]:
+
+
+s = a.len()
+ind /= 2
+
+d = a
+d[ind:s-1:]
+print(d)
+
+
+# In[ ]:
+
+
+
+
